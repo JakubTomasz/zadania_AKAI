1839bc544d605eacefdc05e94c2ba9d90595f88c
JakubTomasz <jakub0tomaszewski@gmail.com>
2023-03-13 19:49:16 +0100

    .

--- python/tasker/src/json/Exporter.py
+++ python/tasker/src/json/Exporter.py
@@ -6,6 +6,9 @@ class Exporter:
     def __init__(self):
         pass
 
-    def save_tasks(self, tasks):
+    def save_tasks(self, tasks,tasks_file = "taski.json"):
         # TODO zapisz taski do pliku tutaj
+        file = open(tasks_file, "w")
+        json.dump(tasks, file, indent=1)
+        file.close()
         pass
--- python/tasker/src/json/Importer.py
+++ python/tasker/src/json/Importer.py
@@ -4,12 +4,16 @@ import json
 class Importer:
 
     def __init__(self):
+        self.tasks = []
         pass
 
-    def read_tasks(self):
+    def read_tasks(self,tasks_file = "taski.json"):
         # TODO odczytaj plik i zdekoduj treść tutaj
+        ratios = open(tasks_file, "r")
+        self.tasks = json.load(ratios)
         pass
 
     def get_tasks(self):
         # TODO zwróć zdekodowane taski tutaj
+        return self.tasks
         pass
