86c0979118d3f680697a342c601ff03b036f82dc
JakubTomasz <jakub0tomaszewski@gmail.com>
2023-03-13 20:04:29 +0100

    .

--- python/currency-converter/converter.py
+++ python/currency-converter/converter.py
@@ -65,18 +65,20 @@ def convert_currency(base_currency, target_currency, amount=1):
             break
         except:
             ratios.close()
-            dump_json_to_file([])
+            dump_json_to_file([]) #The file is probably deleted, this is creating new one
+            if i == 1:
+                print("Problem with opening file")
+                return 0
         ratios.close()
-        if i == 1:
-            print("Problem with opening file")
-            return 0
+
 
     for exchange in data:
         if base_currency == exchange["base_currency"] and target_currency == exchange["target_currency"]:
             return amount * change_exchange_in_ratios(base_currency, target_currency, exchange, data)
-        # I assume the ratio between currecies is an invers (PLN->EUR_ratio == 1 / EUR->PLN_ratio)
+        # I assume the ratio between currecies is an invers (PLN->EUR_ratio == 1 / EUR->PLN_ratio) if this is wrong the 3 lines below can be deleted
         if base_currency == exchange["target_currency"] and target_currency == exchange["base_currency"]:
-            return amount / change_exchange_in_ratios(base_currency, target_currency, exchange, data)
+            ratio = change_exchange_in_ratios(base_currency, target_currency, exchange, data)
+            return amount/ratio if ratio != 0 else 0
     new_exchange = get_currency_ratio(base_currency, target_currency)
     if new_exchange == {}:
         return 0
@@ -85,10 +87,8 @@ def convert_currency(base_currency, target_currency, amount=1):
     return amount * new_exchange["ratio"]
 
 
-
 if __name__ == "__main__" and len(sys.argv) == 4:
     converted_amount = 0
-    # print(sys.argv[2], sys.argv[3], sys.argv[1])
     try:
         converted_amount = convert_currency(sys.argv[2], sys.argv[3], float(sys.argv[1]))
     except:
